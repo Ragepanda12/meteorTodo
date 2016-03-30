8947a706ce80ae00ff790d0965947bf9f23f9aa9
Mendel Liang <mendel.liang@live.com.au>
2016-03-31 01:59:36 +1100

    Initial Commit

---
+++ imports/ui/body.js
@@ -0,0 +1,41 @@
+import { Meteor } from 'meteor/meteor';
+import { Template } from 'meteor/templating';
+import { ReactiveDict } from 'meteor/reactive-dict';
+import { Tasks } from '../api/tasks.js';
+import './task.js';
+import './body.html';
+
+Template.body.helpers({
+    tasks() {
+        const instance = Template.instance();
+        if (instance.state.get('hideCompleted')){
+            return Tasks.find({ checked: { $ne: true } }, { sort: {createdAt: -1 } });
+        }
+        return Tasks.find({}, { sort: { createdAt: -1 } });
+    },
+    incompleteCount(){
+        return Tasks.find({ checked: { $ne: true } }).count();
+    },
+});
+
+Template.body.onCreated(function bodyOnCreated(){
+    this.state = new ReactiveDict();
+    Meteor.subscribe('tasks');
+});
+
+Template.body.events({
+    'submit .new-task'(event) {
+        //prevent default browser form submit
+        event.preventDefault();
+        //get value from form element
+        const target = event.target;
+        const text = target.text.value;
+        //insert task into collection
+        Meteor.call('tasks.insert', text);
+        //clear form
+        target.text.value='';
+    },
+    'change .hide-completed input'(event, instance){
+        instance.state.set('hideCompleted', event.target.checked);
+    },
+});
